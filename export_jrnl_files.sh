b1556db4b72a5948d055945e37717d86734fac7d
Kelley Graham <kelley@durastudio.com>
2020-05-01 11:45:41 -0700

    updated directions in comments

--- export_jrnl_files.sh
+++ export_jrnl_files.sh
@@ -2,11 +2,14 @@
 # Simple jrnl export script
 # Requires jrnl to be installed and configured
 # Does not git add, commit or push files.
-# Automate jrnl name and path from ~/.jrnl-config. Git repo and jrnl have same string.
-# Run from repository via cron, ascends one directory called 'GIT' and exports jrnl files.
+# My setup is simple and uncomplicated. I don't need anything fancier.
+# Automate jrnl name and path from ~/.jrnl-config. Git repo name and jrnl name are the same and must already exist. 
+# Run in any working git repo to export all current journals files in md format. Script expects all repo enabled journals to be at the same level.
 # eg (when pasting into crontab, be sure to verify tab separation)
 ##min    hour    mday    month   wday    command
 #0       6,14,20 *       *       *       sh <your_path_to_/jrnl-rific>/export-jrnl-files.sh 2>&1
+
+#cd path/to/jrnl/repos/parent/dir
 
 for j in $(jrnl -ls) ; do 
     jrnl $j --export txt -o ../$j/
